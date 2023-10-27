e1d76ae1814da05a2f6b1e90a5f7588a80f1bd8b
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-10-27 14:56:48 -0300

    feat: login with git and google (#16)

--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { singInPost } from '@/controllers';
+import { singInPost, singInGoogle, singInGit } from '@/controllers';
 import { validateBody } from '@/middlewares';
 import { signInSchema } from '@/schemas';
 
 const authenticationRouter = Router();
 
-authenticationRouter.post('/sign-in', validateBody(signInSchema), singInPost);
+authenticationRouter
+  .post('/sign-in', validateBody(signInSchema), singInPost)
+  .post('/google', singInGoogle)
+  .post('/git', singInGit);
 
 export { authenticationRouter };
