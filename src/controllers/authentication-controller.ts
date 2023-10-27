abc061f9edda6563563d2b73cfadd8b4f1174e14
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-10-27 12:39:04 -0300

    feat: login with git and google

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { authenticationService, SignInParams } from '@/services';
+import { authenticationService, SignInParams, AuthGitOrGoogle } from '@/services';
 
 export async function singInPost(req: Request, res: Response) {
   const { email, password } = req.body as SignInParams;
 
   const result = await authenticationService.signIn({ email, password });
 
+  return res.status(httpStatus.OK).send(result);
+}
+
+export async function singInGoogle(req: Request, res: Response) {
+  const { code } = req.body as AuthGitOrGoogle;
+  const result = await authenticationService.signInGoogle(code);
+  return res.status(httpStatus.OK).send(result);
+}
+
+export async function singInGit(req: Request, res: Response) {
+  const { code } = req.body as AuthGitOrGoogle;
+  const result = await authenticationService.signInGit(code);
   return res.status(httpStatus.OK).send(result);
 }
--- tests/factories/events-factory.ts
+++ tests/factories/events-factory.ts
@@ -7,7 +7,7 @@ export function createEvent(params: Partial<Event> = {}): Promise<Event> {
   return prisma.event.create({
     data: {
       title: params.title || faker.lorem.sentence(),
-      backgroundImageUrl: params.backgroundImageUrl || faker.image.imageUrl(),
+      backgroundImageUrl: params.backgroundImageUrl || faker.image.url(),
       logoImageUrl: params.logoImageUrl || faker.image.url(),
       startsAt: params.startsAt || dayjs().subtract(1, 'day').toDate(),
       endsAt: params.endsAt || dayjs().add(5, 'days').toDate(),
